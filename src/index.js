1eccb89fa2b8cd86e1c3c916f0b7dab3bb0e4805
sinentantiso <103891865+sinentantiso@users.noreply.github.com>
2022-11-06 14:16:58 +0200

    1 file modified

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
