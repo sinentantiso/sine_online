f25f784ad89fe2c1cc85e5140f2062e4bb629197
sinentantiso <103891865+sinentantiso@users.noreply.github.com>
2022-11-06 13:46:28 +0200

    1 file modified

--- src/Routers/Routers.js
+++ src/Routers/Routers.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Cart from "../Pages/Cart";
+import Checkout from "../Pages/Checkout";
 import Home from "../Pages/Home";
+import Login from "../Pages/Login";
+import ProductDetails from "../Pages/ProductDetails";
+import Shop from "../Pages/Shop";
+import Signup from "../Pages/Signup";
 
 
 const Routers = () => {
