7f4f4b5cea105521c7d06c218939a4b032ca56b1
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-11-06 19:46:53 +0200

    initial

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,45 @@
 import React from "react";
 import "./Header.css";
 
-import {logo} from "../../Assets/images";
+import {NavLink} from "react-router-dom";
+import logo from "../../Assets/images/eco-logo.png";
 import {container, Row } from "reactstrap";
 
 const Header = () => {
   return <header className="header">
     <container>
-      <div className="nav__wrapper"> 
-        <div className="logo">
+      <Row>
+        <div className="nav__wrapper"> 
+          <div className="logo">
+            <img src={logo} alt="logo"/>
+            <div>
+              <h1>Sine-Online</h1>
+              <p>since 2022</p>
+            </div>
+          </div>
 
+            <div className="navigation">
+              <ul className="menu">
+                <li className="nav__item">
+                  <NavLink to="home">Home</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="shop">Shop</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="cart">Cart</NavLink>
+                </li>
+              </ul>
+            </div>
+
+            <div className="Nav__icons">
+
+              <span className="fav__icon"><i class="ri-heart-fill"></i></span>
+              <span className="cart__icon"><i 
+              class="ri-shopping-bag-fill"></i></span>
+            </div>
         </div>
-      </div>
+      </Row>
     </container>   
   </header>
 }
