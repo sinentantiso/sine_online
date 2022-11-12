9b5a3db92b42d45bf6abbac3f2f5160c50d1bdfc
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-11-12 23:21:29 +0200

    Components build

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 
+import {Link} from "react-router-dom";
+import {motion} from "framer-motion";
+
+import Helmet from "../Components/Helmet/Helmet";
+import { Container, Row,Col } from "reactstrap";
+
+
 const Home = () => {
 
   const year = new Date().getFullYear();
 
   return(
-    <helmet title={"home"}>
-      <container>
+    <Helmet title={"home"}>
+      <Container>
         <Row>
           <Col lg="6" mg="6">
             <div className="hero__content">
               <p className="hero__subtitle">Trending product in {year}</p>
-              
+
             </div>
           </Col>
         </Row>
-      </container>
-    </helmet>
+      </Container>
+    </Helmet>
   )
 }
 
---
+++ src/Services/Services.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {motion} from "framer-motion"
+
+const Services () =>{
+    return( 
+    <Section className="service">
+        <Container>
+            <Row>
+              <Col lg="3" md="4">
+                <div className="services__item">
+                 
+                </div>
+              </Col>
+            </Row>
+        </Container>
+    </Section>
+    )
+}
+
+export default Services;
