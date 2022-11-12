ba93b6a543d99ca2d8bc5b2c1b86a31e061d3939
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-11-12 23:55:09 +0200

    Service commit

--- src/Services/Services.jsx
+++ src/Services/Services.jsx
@@ -12,8 +12,11 @@ const Services () =>{
             <Row>
             {
                 serviceData.map((item,index)=>(
-                <Col lg="3" md="4">
-                    <div className="services__item">
+                <Col lg="3" md="4" key={index}>
+                    <motion.div 
+                       whileHover={{scale:1.1}}
+                       className="services__item" 
+                       style={{backgroundColor=`${item.bg}`}}>
                         <span>
                             <i className={item.icon}></i>
                         </span>
@@ -21,7 +24,7 @@ const Services () =>{
                          <h3>{item.title}</h3>
                          <p>{item.subtitle}</p>
                         </div>
-                    </div>
+                    </motion.div>
                 </Col>            
                 ))
             }
