2d51ca59494098ad65b6046756be205b2ff238c7
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-11-13 14:57:55 +0200

    update

--- src/Components/UI/ProductCard.jsx
+++ src/Components/UI/ProductCard.jsx
@@ -5,9 +5,30 @@ import "../../styles/product-card.css";
 import { Col } from "reactstrap";
 import { Link } from "react-router-dom";
 
-const ProductCard = () => {
+const ProductCard = ({item}) => {
     return (
-      <div className="product__item"
-
+      <Col lg="3" mg="4" className="mb-2">
+        <div className="product__item">
+            <div className="product__img">
+                <motion.img whileHover={{scale:0.9}} src={item.imgUrl} 
+                alt=""/>
+            </div>
+            <div className="p-2 product__info">
+                <h3 className="product__name">
+                    <Link to={`/shop/${item.id}`}>{item.productName}</Link>
+                </h3>
+                <span>{item.catergory}</span>
+            </div>
+            <div className="product__card-bottom d-flex align-items-center 
+            justify-content-between p-2">
+                <span className="price">${item.price}</span>
+                <motion.span whileTap={{scale: 1.2}}>
+                    <i class="ri-add-line"></i>
+                </motion.span>
+            </div>
+        </div>
+      </Col>
     )
 }
+
+export default ProductCard;
