c9ddb57802190626c44596ec8dc7bc816f599d49
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-11-13 13:48:36 +0200

    123

---
+++ src/Components/UI/ProductCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import productImg from "../../assets/images/arm-chair-01.jpg";
+
+const ProductCard = () => {
+    return (
+      <div className="product__item"
+
+    )
+}
